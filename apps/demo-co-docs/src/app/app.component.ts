6866e49aeb21a185bbd907bca366be51998e9bd5
nayfin <natjkern@gmail.com>
2020-01-31 09:30:05 -0600

    adds dependancies and sidenave (build failing)

--- apps/demo-co-docs/src/app/app.component.ts
+++ apps/demo-co-docs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidenavSection } from '@tft/core';
 
 @Component({
   selector: 'demo-co-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-co-docs';
+
+  appNavTree: SidenavSection[] = [
+    {
+      title: 'CRISPR Forms',
+      subtitle: 'DNA driven forms',
+      description: `Overview of CRISPR Forms`,
+      links: [
+        {
+          title: 'Overview',
+          path: 'crispr-forms-demo/overview',
+        }
+      ],
+    }
+  ]
+  onLinkSelected(event) {
+    console.log(event);
+  }
 }
--- apps/demo-co-docs/src/app/app.module.ts
+++ apps/demo-co-docs/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormFieldsModule } from '@demo-co/form-fields';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '@tft/core';
+import { UiImportsModule } from '@tft/ui-imports';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormFieldsModule
+    FormFieldsModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    UiImportsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
