8da90397016bb56c885fc377cc310c5e9052281d
nayfin <natjkern@gmail.com>
2020-02-12 21:19:22 -0600

    Revert "feat: 🎸 ties route path to subtitle of header"
    This reverts commit 0972d04aa8331d5e03ceb49959313c2f061bd459.

--- apps/demo-co-docs/src/app/app.component.ts
+++ apps/demo-co-docs/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SidenavSection } from '@tft/core';
-import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
-import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
  /**
   * @example
@@ -14,9 +11,8 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'demo-co-docs';
-  subtitle$: Observable<string>;
 
   linksToExamples: SidenavSection[] = [
     {
@@ -31,18 +27,7 @@ export class AppComponent implements OnInit {
       ],
     }
   ];
-  constructor(
-    private router: Router,
-  ) { }
-  ngOnInit() {
-    this.subtitle$ = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(endEvent => {
-        return (endEvent as NavigationEnd).url
-      })
-    );
 
-  }
   onLinkSelected(event) {
     console.log(event);
   }
